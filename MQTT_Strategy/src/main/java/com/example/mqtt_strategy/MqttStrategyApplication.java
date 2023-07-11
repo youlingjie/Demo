e7bad903aa8e28053e5c410903e5add9c5ac503e
youlingjie <you-lingjie@outlook.com>
2023-07-11 15:28:47 +0800

    Initial commit

---
+++ MQTT_Strategy/src/main/java/com/example/mqtt_strategy/MqttStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mqtt_strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttStrategyApplication.class, args);
+	}
+
+}
---
+++ MQTT_Strategy/src/test/java/com/example/mqtt_strategy/MqttStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqtt_strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
